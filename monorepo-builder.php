90a9be27f3beaaa2fb7541e6334f976333f4a056
Andreas Mischke <andreas.mischke@prym.com>
2022-10-16 22:11:21 +0200

    Initial commit

---
+++ monorepo-builder.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+use Symplify\MonorepoBuilder\Config\MBConfig;
+use Symplify\MonorepoBuilder\Contract\Git\TagResolverInterface;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushNextDevReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\PushTagReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualConflictsReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetCurrentMutualDependenciesReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\SetNextMutualDependenciesReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\TagVersionReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateBranchAliasReleaseWorker;
+use Symplify\MonorepoBuilder\Release\ReleaseWorker\UpdateReplaceReleaseWorker;
+
+return static function (MBConfig $mbConfig): void {
+    $mbConfig->packageDirectories([
+        __DIR__  . "/packages",
+        __DIR__  . "/plugins",
+    ]);
+    $mbConfig->defaultBranch('main');
+
+    $mbConfig->dataToAppend([
+        'require-dev' => [
+            'symplify/monorepo-builder' => '^11.1',
+        ],
+    ]);
+
+    $mbConfig->workers([
+        UpdateReplaceReleaseWorker::class,
+        SetCurrentMutualConflictsReleaseWorker::class,
+        SetCurrentMutualDependenciesReleaseWorker::class,
+        TagVersionReleaseWorker::class,
+        PushTagReleaseWorker::class,
+        SetNextMutualDependenciesReleaseWorker::class,
+        UpdateBranchAliasReleaseWorker::class,
+        PushNextDevReleaseWorker::class,
+    ]);
+};
